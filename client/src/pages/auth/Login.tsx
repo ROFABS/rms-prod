c6c592f3048614cef8abce66df06eacc8bc97ded
ROFABS <159296862+ROFABS@users.noreply.github.com>
2025-01-04 17:39:06 +0530

    Login.tsx

--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -266,7 +266,7 @@ function Login() {
       setUser(response.data.user); // Assuming you have a setUser function in your global context
 
       toast.success("Logged in automatically");
-      navigate("/channelsmenu"); // Redirect to the dashboard
+      navigate("/ksr"); // Redirect to the dashboard
     } catch (err) {
       console.error("Auto-login failed", err);
       toast.error("Auto-login failed. Please try again.");
@@ -278,7 +278,7 @@ function Login() {
     const localData = JSON.parse(localStorage.getItem("_session") as string);
     if (localData && localData.user) {
       // If session exists, redirect directly
-      navigate("/channelsmenu");
+      navigate("/ksr");
     } else {
       // Otherwise, attempt to login automatically
       loginAutomatically();
