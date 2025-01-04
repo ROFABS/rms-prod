15dd1ee13b828f46034b0defb1470ddf38d7e872
ROFABS <159296862+ROFABS@users.noreply.github.com>
2025-01-04 16:22:45 +0530

    Update Login.tsx

--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -266,7 +266,7 @@ function Login() {
       setUser(response.data.user); // Assuming you have a setUser function in your global context
 
       toast.success("Logged in automatically");
-      navigate("/dashboard"); // Redirect to the dashboard
+      navigate("/ksr"); // Redirect to the dashboard
     } catch (err) {
       console.error("Auto-login failed", err);
       toast.error("Auto-login failed. Please try again.");
