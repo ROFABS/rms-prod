6f1db15937bc6b4a60ce9b1e12b5926aee1720b1
ROFABS <159296862+ROFABS@users.noreply.github.com>
2025-01-05 21:23:09 +0530

    Update Root.tsx

--- client/src/components/layout/Root.tsx
+++ client/src/components/layout/Root.tsx
@@ -34,7 +34,7 @@ const Root = () => {
 
   useEffect(() => {
     if (pathname === "/") {
-      navigate("/ksr");
+      navigate("/dashboard");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname]);
